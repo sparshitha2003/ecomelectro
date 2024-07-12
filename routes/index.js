b51ba6f01f0e8df8fbe18725ed40873c71ea5be2
sparshitha2003 <19301sparshithasdevadiga@gmail.com>
2024-07-12 19:39:57 +0530

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+
+const userSignUpController=require("../controller/userSignup")
+ router.post("/signup", userSignUpController)
+
+module.exports=router;
